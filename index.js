ba92f9c7093fd41c043ca1afc15e451c92d9f591
Thomas Völkel <thomas.voelkel@hdnet.de>
2024-05-17 11:02:18 +0200

    chore(git-plugin): Add composer and yarn files; restructure plugin adds

--- index.js
+++ index.js
@@ -1,68 +1,92 @@
-export default {
-    branches: ['main', 'next'],
-    tagFormat: '${version}',
-    plugins: [
-        [
-            '@semantic-release/commit-analyzer',
-            {
-                preset: 'conventionalcommits',
-                releaseRules: [
-                    { breaking: true, release: 'major' },
-                    { type: 'build', release: 'patch' },
-                    { type: 'chore', release: 'patch' },
-                    { type: 'ci', release: 'patch' },
-                    { type: 'docs', release: 'patch' },
-                    { type: 'dx', release: 'patch' },
-                    { type: 'feat', release: 'minor' },
-                    { type: 'fix', release: 'patch' },
-                    { type: 'perf', release: 'minor' },
-                    { type: 'refactor', release: 'patch' },
-                    { type: 'revert', release: 'minor' },
-                    { type: 'style', release: 'patch' },
-                    { type: 'test', release: 'patch' },
-                    { type: 'types', release: 'patch' },
-                ],
-                parserOpts: {
-                    noteKeywords: ['BREAKING CHANGE', 'BREAKING CHANGES'],
-                },
-            },
-        ],
-        [
-            '@semantic-release/release-notes-generator',
-            {
-                preset: 'conventionalcommits',
-                presetConfig: {
-                    commitUrlFormat: '{{host}}/{{owner}}/{{repository}}/commits/{{hash}}',
-                    compareUrlFormat: '{{host}}/{{owner}}/{{repository}}/compare/{{currentTag}}%0D{{previousTag}}',
-                    issueUrlFormat: 'https://hdgroup.atlassian.net/browse/{{prefix}}{{id}}',
-                    issuePrefixes: ['#'],
-                },
-            },
-        ],
-        [
-            '@semantic-release/changelog',
-            {
-                changelogTitle: '# Changelog\n',
-            },
-        ],
-        [
-            '@semantic-release/npm',
-            {
-                npmPublish: false,
-            },
-        ],
-        [
-            '@semantic-release/git',
-            {
-                assets: ['CHANGELOG.md', 'package.json', 'package-lock.json'],
-                message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
-            },
-        ],
-        [
-            '@semantic-release/exec',
-            {
-                successCmd: 'echo "${nextRelease.version}" > release.properties',
-            },
-        ],
+import { existsSync } from 'node:fs'
+
+const appendFileIfExists = (filename) => (existsSync(filename) ? [filename] : [])
+
+const createPluginCommitAnalyzer = () => [
+  '@semantic-release/commit-analyzer',
+  {
+    preset: 'conventionalcommits',
+    releaseRules: [
+      { breaking: true, release: 'major' },
+      { type: 'build', release: 'patch' },
+      { type: 'chore', release: 'patch' },
+      { type: 'ci', release: 'patch' },
+      { type: 'docs', release: 'patch' },
+      { type: 'dx', release: 'patch' },
+      { type: 'feat', release: 'minor' },
+      { type: 'fix', release: 'patch' },
+      { type: 'perf', release: 'minor' },
+      { type: 'refactor', release: 'patch' },
+      { type: 'revert', release: 'minor' },
+      { type: 'style', release: 'patch' },
+      { type: 'test', release: 'patch' },
+      { type: 'types', release: 'patch' },
     ],
+    parserOpts: {
+      noteKeywords: ['BREAKING CHANGE', 'BREAKING CHANGES'],
+    },
+  },
+]
+
+const createPluginReleaseNotesGenerator = () => [
+  '@semantic-release/release-notes-generator',
+  {
+    preset: 'conventionalcommits',
+    presetConfig: {
+      commitUrlFormat: '{{host}}/{{owner}}/{{repository}}/commits/{{hash}}',
+      compareUrlFormat: '{{host}}/{{owner}}/{{repository}}/compare/{{currentTag}}%0D{{previousTag}}',
+      issueUrlFormat: 'https://hdgroup.atlassian.net/browse/{{prefix}}{{id}}',
+      issuePrefixes: ['#'],
+    },
+  },
+]
+
+const createPluginChangelog = () => [
+  '@semantic-release/changelog',
+  {
+    changelogTitle: '# Changelog\n',
+  },
+]
+
+const createPluginNpm = () => [
+  '@semantic-release/npm',
+  {
+    npmPublish: false,
+  },
+]
+
+const createPluginGit = () => [
+  '@semantic-release/git',
+  {
+    assets: [
+      'CHANGELOG.md',
+      ...appendFileIfExists('composer.json'),
+      ...appendFileIfExists('composer.lock'),
+      ...appendFileIfExists('package.json'),
+      ...appendFileIfExists('package-lock.json'),
+      ...appendFileIfExists('yarn.lock'),
+    ],
+    message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+  },
+]
+
+const createPluginExec = () => [
+  '@semantic-release/exec',
+  {
+    successCmd: 'echo "${nextRelease.version}" > release.properties',
+  },
+]
+
+export default {
+  branches: ['main', 'next'],
+  tagFormat: '${version}',
+  plugins: [
+    // order matters!
+    createPluginCommitAnalyzer(),
+    createPluginReleaseNotesGenerator(),
+    createPluginChangelog(),
+    createPluginNpm(),
+    createPluginGit(),
+    createPluginExec(),
+  ],
 }
